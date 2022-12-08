651ca2c98d0065fcaa32d6d888b62a48c9f93d4b
sharayura <sharapyura@gmail.com>
2022-12-08 16:01:36 +0300

    Initial commit

---
+++ src/test/java/org/skypro/cw3/Cw3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.cw3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cw3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
